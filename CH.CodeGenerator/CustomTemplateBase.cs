d5b2000d98ec3022e2155cbafb3e13cca4996aad
chenl <273168121@qq.com>
2017-06-18 19:50:09 +0800

    增加代码生成器

---
+++ CH.CodeGenerator/CustomTemplateBase.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RazorEngine.Templating;
+using LinqToDB.SchemaProvider;
+using LinqToDB.SchemaProvider;
+using LinqToDB.Data;
+namespace CH.CodeGenerator
+{
+    public class CustomTemplateBase<T> : TemplateBase<T>
+    {
+
+        public CustomTemplateBase()
+        {
+
+            this.HtmlHelper = new HtmlHelper();
+        }
+
+        public HtmlHelper HtmlHelper { get; set; }
+
+    }
+
+    public class HtmlHelper
+    {
+        public string ToUpper(string str)
+        {
+            return str.ToUpper();
+        }
+
+        //[Column(Name = "Action_Id", CanBeNull = false)]
+        private string ColumnProperty(ColumnSchema col)
+        {
+
+            string rs = string.Empty;
+
+            if(col.IsPrimaryKey)
+            {
+                rs += "[PrimaryKey]\r\n";
+            } 
+            rs += string.Format("[Column(Name=\"{0}\",CanBeNull={1},IsIdentity={2}", col.ColumnName.ToUpper(), (!col.IsNullable).ToString().ToLower(),col.IsIdentity.ToString().ToLower());  
+
+            return rs;
+
+        }
+
+
+         
+       
+
+        /// <summary>
+        /// 转换成Pascal字符串
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public string ToPascal(string str)
+        {
+            var separator = new[] { '_', '-' };
+
+            StringBuilder sb = new StringBuilder();
+            string[] parts = str.Split(separator);
+            foreach (string part in parts)
+            {
+                if (part.Length > 0)
+                {
+                    sb.Append(Char.ToUpper(part[0]));
+                    if (part.Length > 1) sb.Append(part.Substring(1).ToLower());
+                }
+            }
+
+            return sb.ToString();
+        }
+
+
+      
+    }
+}
---
+++ CH.CodeGenerator/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace CH.CodeGenerator
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new frmMain());
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+    /*
+     
+     static void Main(string[] args)
+        {
+           var db = new LinqToDB.Data.DataConnection("SqlServer");
+
+            var sp = db.DataProvider.GetSchemaProvider();
+            var schema = sp.GetSchema(db);
+
+            LinqToDB.SchemaProvider.TableSchema table = null;
+
+            foreach (var item in schema.Tables)
+            {
+                if (item.TableName.ToUpper() == "SYS_SIGIN_LOG")
+                {
+
+
+                    table = item;
+                    foreach (var col in item.Columns)
+                    {
+                       
+                        //Console.WriteLine(string.Format(" col.ColumnName:{0},col.ColumnType:{1},col.DataType :{2},col.Length:{3} ,col.MemberType:{4}", col.ColumnName, col.ColumnType, col.DataType, col.Length, col.MemberType));
+                    }
+                }
+            }
+
+ 
+            string index = System.IO.File.ReadAllText("testdb2linq.cshtml", System.Text.Encoding.UTF8);
+            var config = new TemplateServiceConfiguration();
+            config.BaseTemplateType = typeof(CustomTemplateBase<>);
+            //config.Debug = true;
+            using (var service = RazorEngineService.Create(config))
+            {
+
+
+                string result = service.RunCompile(index, string.Empty, null,  new  { Table=table,Cols=table.Columns }); 
+               
+                //Console.WriteLine(result);
+
+                using (FileStream fs = new FileStream(table.TableName.ToUpper().ToPascal() + ".cs", FileMode.Create))
+                {
+                   using( StreamWriter sw = new StreamWriter(fs))
+                   {
+                       sw.Write(result);
+                   }
+                  
+                }
+
+               
+            } 
+        
+        }
+     */
+
+
+
+
+
+
+}
